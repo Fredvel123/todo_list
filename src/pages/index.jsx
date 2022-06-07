c8695b8970b193a12e0aba6e332da9a35e2eae7d
Fredvel123 <freddyvelarde59@gmail.com>
2022-06-06 22:13:17 -0400

    cross origin problem solved

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         {/* fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200&display=swap"
           rel="stylesheet"
